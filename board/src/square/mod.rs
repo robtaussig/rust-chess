caee80ce3681241fec6863250ee5b86d14267f56
Rob Taussig <robert.taussig@gmail.com>
2018-12-19 21:49:46 -0500

    Use shared functions for white and black pieces, implement bishop and rook moves

--- board/src/square/mod.rs
+++ board/src/square/mod.rs
@@ -43,27 +43,13 @@ impl Piece {
     }
 
     pub fn get_moves(&self, board_index: usize, board: Rc<Board>) -> Vec<Move> {
-        match self.color {
-            Color::White => {
-                match self.piece_type {
-                    PieceType::Pawn => valid_moves::get_white_pawn_moves(board_index as i8, board),
-                    PieceType::Knight => valid_moves::get_white_knight_moves(board_index as i8, board),
-                    PieceType::Bishop => valid_moves::get_white_bishop_moves(board_index as i8, board),
-                    PieceType::Rook => valid_moves::get_white_rook_moves(board_index as i8, board),
-                    PieceType::Queen => valid_moves::get_white_queen_moves(board_index as i8, board),
-                    PieceType::King => valid_moves::get_white_king_moves(board_index as i8, board),
-                }
-            },
-            Color::Black => {
-                match self.piece_type {
-                    PieceType::Pawn => valid_moves::get_black_pawn_moves(board_index as i8, board),
-                    PieceType::Knight => valid_moves::get_black_knight_moves(board_index as i8, board),
-                    PieceType::Bishop => valid_moves::get_black_bishop_moves(board_index as i8, board),
-                    PieceType::Rook => valid_moves::get_black_rook_moves(board_index as i8, board),
-                    PieceType::Queen => valid_moves::get_black_queen_moves(board_index as i8, board),
-                    PieceType::King => valid_moves::get_black_king_moves(board_index as i8, board),
-                }
-            },
+        match self.piece_type {
+            PieceType::Pawn => valid_moves::get_pawn_moves(board_index as i8, board, self.color),
+            PieceType::Knight => valid_moves::get_knight_moves(board_index as i8, board, self.color),
+            PieceType::Bishop => valid_moves::get_bishop_moves(board_index as i8, board, self.color),
+            PieceType::Rook => valid_moves::get_rook_moves(board_index as i8, board, self.color),
+            PieceType::Queen => valid_moves::get_queen_moves(board_index as i8, board, self.color),
+            PieceType::King => valid_moves::get_king_moves(board_index as i8, board, self.color),
         }
     }
 }
@@ -117,7 +103,6 @@ pub mod valid_moves {
     const KNIGHT_STEPPING_MOVES: [i8; 8] = [-12, -21, -19, -8, 12, 21, 19, 8];
     #[allow(unused)]
     const KING_QUEEN_MOVE_DIRECTIONS: [i8; 8] = [-1, -11, -10, -9, 1, 11, 10, 9];
-    #[allow(unused)]
     const ROOK_MOVE_DIRECTIONS: [i8; 4] = [-1, 1, -10, 10];
     #[allow(unused)]
     const WHITE_PAWN_MOVE_DIRECTIONS: [i8; 1] = [-10];
@@ -129,23 +114,18 @@ pub mod valid_moves {
     }
 
     #[allow(unused)]
-    pub fn get_white_pawn_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
+    pub fn get_pawn_moves(board_index: i8, board: Rc<Board>, color: Color) -> Vec<Move> {
         
         Vec::new()
     }
 
-    #[allow(unused)]
-    pub fn get_black_pawn_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        Vec::new()
-    }
-
-    pub fn get_white_knight_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
+    pub fn get_knight_moves(board_index: i8, board: Rc<Board>, color: Color) -> Vec<Move> {
         let legal_moves: Vec<Move> = KNIGHT_STEPPING_MOVES.into_iter()
             .map(|step| board_index + step)
             .filter(|to| {
                 if is_out_of_bounds(*to) { return false; }
                 match board.get_piece_at(*to as usize) {
-                    Some(p) => p.color == Color::Black,
+                    Some(p) => p.color != color,
                     None => true
                 }
             })
@@ -155,59 +135,63 @@ pub mod valid_moves {
         legal_moves
     }
 
-    pub fn get_black_knight_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        let legal_moves: Vec<Move> = KNIGHT_STEPPING_MOVES.into_iter()
-            .map(|step| board_index + step)
-            .filter(|to| {
-                if is_out_of_bounds(*to) { return false; }
-                match board.get_piece_at(*to as usize) {
-                    Some(p) => p.color == Color::White,
-                    None => true
+    pub fn get_bishop_moves(board_index: i8, board: Rc<Board>, color: Color) -> Vec<Move> {
+        let mut legal_moves: Vec<Move> = Vec::new();
+
+        for direction in BISHOP_MOVE_DIRECTIONS.into_iter() {
+            let mut to: i8 = board_index;
+            loop {
+                to = to + direction;
+                if is_out_of_bounds(to) { break; }
+                match board.get_piece_at(to as usize) {
+                    Some(p) => {
+                        if p.color != color {
+                            legal_moves.push(Move::new(board_index as usize, to as usize));
+                        }
+                        break;
+                    },
+                    None => {
+                        legal_moves.push(Move::new(board_index as usize, to as usize));
+                    },
                 }
-            })
-            .map(|to| Move::new(board_index as usize, to as usize))
-            .collect();
+            }
+        }
+
+        legal_moves
+    }
+
+    pub fn get_rook_moves(board_index: i8, board: Rc<Board>, color: Color) -> Vec<Move> {
+        let mut legal_moves: Vec<Move> = Vec::new();
+
+        for direction in ROOK_MOVE_DIRECTIONS.into_iter() {
+            let mut to: i8 = board_index;
+            loop {
+                to = to + direction;
+                if is_out_of_bounds(to) { break; }
+                match board.get_piece_at(to as usize) {
+                    Some(p) => {
+                        if p.color != color {
+                            legal_moves.push(Move::new(board_index as usize, to as usize));
+                        }
+                        break;
+                    },
+                    None => {
+                        legal_moves.push(Move::new(board_index as usize, to as usize));
+                    },
+                }
+            }
+        }
 
         legal_moves
     }
 
     #[allow(unused)]
-    pub fn get_white_bishop_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
+    pub fn get_queen_moves(board_index: i8, board: Rc<Board>, color: Color) -> Vec<Move> {
         Vec::new()
     }
 
     #[allow(unused)]
-    pub fn get_black_bishop_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        Vec::new()
-    }
-
-    #[allow(unused)]
-    pub fn get_white_rook_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        Vec::new()
-    }
-
-    #[allow(unused)]
-    pub fn get_black_rook_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        Vec::new()
-    }
-
-    #[allow(unused)]
-    pub fn get_white_queen_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        Vec::new()
-    }
-
-    #[allow(unused)]
-    pub fn get_black_queen_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        Vec::new()
-    }
-
-    #[allow(unused)]
-    pub fn get_white_king_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
-        Vec::new()
-    }
-
-    #[allow(unused)]
-    pub fn get_black_king_moves(board_index: i8, board: Rc<Board>) -> Vec<Move> {
+    pub fn get_king_moves(board_index: i8, board: Rc<Board>, color: Color) -> Vec<Move> {
         Vec::new()
     }
 }
@@ -219,7 +203,7 @@ mod tests {
     mod valid_moves {
         use super::*;
 
-        mod get_white_pawn_moves {            
+        mod get_pawn_moves {            
 
             #[test]
             fn it_should_return_a_list_of_moves_from_starting_position() {
@@ -227,41 +211,52 @@ mod tests {
             }
         }
 
-        mod get_black_pawn_moves {            
-
-            #[test]
-            fn it_should_return_a_list_of_moves_from_starting_position() {
-                assert!(true);       
-            }
-        }
-
-        mod get_white_knight_moves {
-            use super::*; 
+        mod get_knight_moves {
+            use super::*;
     
             #[test]
             fn it_should_return_a_list_of_moves_from_starting_position() {
                 let board_string = String::from("00000000000rnbqkbnr00pppppppp00--------00--------00--------00--------00PPPPPPPP00RNBQKBNR00000000000");
                 let board: Board = helpers::generate_board(board_string);
-                let knight = board.get_piece_at(82 as usize).unwrap();
-                let legal_moves: Vec<Move> = knight.get_moves(82 as usize, Rc::new(board));
+                let knight = board.get_piece_at(helpers::square_to_index(String::from("g1")) as usize).unwrap();
+                let legal_moves: Vec<Move> = knight.get_moves(helpers::square_to_index(String::from("g1")) as usize, Rc::new(board));
                 assert_eq!(legal_moves.len(), 2);
             }
         }
 
-        mod get_black_knight_moves {            
+        mod get_bishop_moves {            
             use super::*;
 
             #[test]
             fn it_should_return_a_list_of_moves_from_starting_position() {
                 let board_string = String::from("00000000000rnbqkbnr00pppppppp00--------00--------00--------00--------00PPPPPPPP00RNBQKBNR00000000000");
-                let board: Board = helpers::generate_board(board_string);
-                let knight = board.get_piece_at(12 as usize).unwrap();
-                let legal_moves: Vec<Move> = knight.get_moves(12 as usize, Rc::new(board));
-                assert_eq!(legal_moves.len(), 2);
+                let mut board: Board = helpers::generate_board(board_string);
+                board.make_move(Move::from_chess_move((String::from("e2"), String::from("e4"))));
+                board.make_move(Move::from_chess_move((String::from("e7"), String::from("e5"))));
+                let bishop = board.get_piece_at(helpers::square_to_index(String::from("f1")) as usize).unwrap();
+                let legal_moves: Vec<Move> = bishop.get_moves(helpers::square_to_index(String::from("f1")) as usize, Rc::new(board));
+                assert_eq!(legal_moves.len(), 5);
             }
         }
 
-        mod get_white_bishop_moves {            
+        mod get_rook_moves {            
+            use super::*;
+
+            #[test]
+            fn it_should_return_a_list_of_moves_from_starting_position() {
+                let board_string = String::from("00000000000rnbqkbnr00pppppppp00--------00--------00--------00--------00PPPPPPPP00RNBQKBNR00000000000");
+                let mut board: Board = helpers::generate_board(board_string);
+                board.make_move(Move::from_chess_move((String::from("h2"), String::from("h4"))));
+                board.make_move(Move::from_chess_move((String::from("e7"), String::from("e5"))));
+                board.make_move(Move::from_chess_move((String::from("g1"), String::from("f3"))));
+                board.make_move(Move::from_chess_move((String::from("d7"), String::from("d5"))));
+                let rook = board.get_piece_at(88 as usize).unwrap();
+                let legal_moves: Vec<Move> = rook.get_moves(88 as usize, Rc::new(board));
+                assert_eq!(legal_moves.len(), 3);
+            }
+        }
+
+        mod get_queen_moves {            
 
             #[test]
             fn it_should_return_a_list_of_moves_from_starting_position() {
@@ -269,55 +264,7 @@ mod tests {
             }
         }
 
-        mod get_black_bishop_moves {            
-
-            #[test]
-            fn it_should_return_a_list_of_moves_from_starting_position() {
-                assert!(true);       
-            }
-        }
-
-        mod get_white_rook_moves {            
-
-            #[test]
-            fn it_should_return_a_list_of_moves_from_starting_position() {
-                assert!(true);       
-            }
-        }
-
-        mod get_black_rook_moves {            
-
-            #[test]
-            fn it_should_return_a_list_of_moves_from_starting_position() {
-                assert!(true);       
-            }
-        }
-
-        mod get_white_queen_moves {            
-
-            #[test]
-            fn it_should_return_a_list_of_moves_from_starting_position() {
-                assert!(true);       
-            }
-        }
-
-        mod get_black_queen_moves {            
-
-            #[test]
-            fn it_should_return_a_list_of_moves_from_starting_position() {
-                assert!(true);       
-            }
-        }
-
-        mod get_white_king_moves {            
-
-            #[test]
-            fn it_should_return_a_list_of_moves_from_starting_position() {
-                assert!(true);       
-            }
-        }
-
-        mod get_black_king_moves {            
+        mod get_king_moves {            
 
             #[test]
             fn it_should_return_a_list_of_moves_from_starting_position() {
