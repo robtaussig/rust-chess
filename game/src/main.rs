1296c3bf0f9f3d5ec3834b023d07f78086fe559b
Rob Taussig <robert.taussig@gmail.com>
2018-12-22 20:00:25 -0500

    Implement single ply move evaluation

--- game/src/main.rs
+++ game/src/main.rs
@@ -11,12 +11,12 @@ fn main() {
     if args.len() == 1 {
         let board_string = String::from("00000000000rnbqkbnr00pppppppp00--------00--------00--------00--------00PPPPPPPP00RNBQKBNR00000000000");
         let mut board: board::Board = generate_board(board_string);
-        let legal_moves: Vec<Move> = get_all_legal_moves(board);
+        let (board, legal_moves) = get_all_legal_moves(board);
         println!("{:?}", legal_moves);
     } else {
         let board_string = String::from(args[1].clone());
         let mut board: board::Board = generate_board(board_string);
-        let legal_moves: Vec<Move> = get_all_legal_moves(board);
+        let (board, legal_moves) = get_all_legal_moves(board);
         println!("{:?}", legal_moves);
     }
 }
