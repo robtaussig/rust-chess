587f66463f7f6f5b18eea02c971502974686d1a6
Rob Taussig <robert.taussig@gmail.com>
2018-12-22 20:02:18 -0500

    Add eval to main function

--- game/src/main.rs
+++ game/src/main.rs
@@ -5,6 +5,7 @@ use std::env;
 use board::helpers::generate_board;
 use board::Move;
 use eval::get_all_legal_moves;
+use eval::get_best_move;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,11 +13,13 @@ fn main() {
         let board_string = String::from("00000000000rnbqkbnr00pppppppp00--------00--------00--------00--------00PPPPPPPP00RNBQKBNR00000000000");
         let mut board: board::Board = generate_board(board_string);
         let (board, legal_moves) = get_all_legal_moves(board);
-        println!("{:?}", legal_moves);
+        let best_move: Move = get_best_move(&board, 1);
+        println!("{:?}, {}", legal_moves, best_move);
     } else {
         let board_string = String::from(args[1].clone());
         let mut board: board::Board = generate_board(board_string);
         let (board, legal_moves) = get_all_legal_moves(board);
-        println!("{:?}", legal_moves);
+        let best_move: Move = get_best_move(&board, 1);
+        println!("{:?}, {}", legal_moves, best_move);
     }
 }
